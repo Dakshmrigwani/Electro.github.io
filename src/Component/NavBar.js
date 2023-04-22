1a0a105715422a3b8b61a1ef199b2b96fe1db0bd
Daksh Mrigwani <dakshmrigwani@gmail.com>
2023-04-22 14:23:49 +0000

    Update 3 files

--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -6,96 +6,103 @@ import Navbar from "react-bootstrap/Navbar";
 import { BsCart3, BsMoonStarsFill, BsSunFill } from "react-icons/bs";
 import { AiOutlineHeart } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 
 function NavBar() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
   return (
     <>
-      <Navbar bg="light" expand="lg" className="sticky-top">
-        <Container fluid>
-          <Navbar.Brand>
-            <Link to="/Pages/Body" className="text-decoration-none text-dark">
-              Electro.
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0 "
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Link
-                to="/Pages/Headphone"
-                className="text-decoration-none text-dark me-2"
+      <nav className={isDarkMode ? "navbar-dark" : "navbar-light"}>
+        <Navbar bg="light" expand="lg" className="sticky-top">
+          <Container fluid>
+            <Navbar.Brand>
+              <Link to="/Pages/Body" className="text-decoration-none text-dark">
+                Electro.
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0 "
+                style={{ maxHeight: "100px" }}
+                navbarScroll
               >
-                Headphone
-              </Link>
-              <Link
-                to="/Pages/Laptops"
-                className="text-decoration-none text-dark me-2"
+                <Link
+                  to="/Pages/Headphone"
+                  className="text-decoration-none text-dark me-2"
+                >
+                  Headphone
+                </Link>
+                <Link
+                  to="/Pages/Laptops"
+                  className="text-decoration-none text-dark me-2"
+                >
+                  Laptops
+                </Link>
+                <Link
+                  to="/Pages/Speaker"
+                  className="text-decoration-none text-dark me-2"
+                >
+                  Speakers
+                </Link>
+                <Link
+                  to="/Pages/Watches"
+                  className="text-decoration-none text-dark me-2"
+                >
+                  Watches
+                </Link>
+                <Link
+                  to="/Pages/Smartphones"
+                  className="text-decoration-none text-dark me-2"
+                >
+                  Smartphones
+                </Link>
+                <Link
+                  to="/Pages/About"
+                  className="text-decoration-none text-dark me-2"
+                >
+                  AboutUs
+                </Link>
+                <Link
+                  to="/Pages/Services"
+                  className="text-decoration-none text-dark"
+                >
+                  Services
+                </Link>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+              <Button
+                className="btn btn-light mb-1"
+                style={{ boxShadow: "none" }}
+                onClick={toggleDarkMode}
               >
-                Laptops
-              </Link>
-              <Link
-                to="/Pages/Speaker"
-                className="text-decoration-none text-dark me-2"
-              >
-                Speakers
-              </Link>
-              <Link
-                to="/Pages/Watches"
-                className="text-decoration-none text-dark me-2"
-              >
-                Watches
-              </Link>
-              <Link
-                to="/Pages/Smartphones"
-                className="text-decoration-none text-dark me-2"
-              >
-                Smartphones
-              </Link>
-              <Link
-                to="/Pages/About"
-                className="text-decoration-none text-dark me-2"
-              >
-                AboutUs
-              </Link>
-              <Link
-                to="/Pages/Services"
-                className="text-decoration-none text-dark"
-              >
-                Services
-              </Link>
-            </Nav>
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-            <Button
-              className="btn btn-light mb-1"
-              style={{ boxShadow: "none" }}
-            >
-              <BsSunFill /> : <BsMoonStarsFill />
-            </Button>
-            <Button className="btn btn-light" style={{ boxShadow: "none" }}>
-              <Link to="/Pages/Wishlist">
-                <AiOutlineHeart className="text-dark fs-4" />
-              </Link>
-            </Button>
-            <Button className="btn btn-light" style={{ boxShadow: "none" }}>
-              <Link to="/pages/Cart">
-                <BsCart3 className="text-dark fs-4" />
-              </Link>
-            </Button>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+                {isDarkMode ? <BsSunFill /> : <BsMoonStarsFill />}
+              </Button>
+              <Button className="btn btn-light" style={{ boxShadow: "none" }}>
+                <Link to="/Pages/Wishlist">
+                  <AiOutlineHeart className="text-dark fs-4" />
+                </Link>
+              </Button>
+              <Button className="btn btn-light" style={{ boxShadow: "none" }}>
+                <Link to="/pages/Cart">
+                  <BsCart3 className="text-dark fs-4" />
+                </Link>
+              </Button>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </nav>
     </>
   );
 }
