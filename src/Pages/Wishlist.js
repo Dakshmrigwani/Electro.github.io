e25e903bb134f62e515432e01b070991ba440ebe
Daksh Mrigwani <dakshmrigwani@gmail.com>
2023-04-25 13:34:24 +0000

    Update Cart.js and Wishlist.js

--- src/Pages/Wishlist.js
+++ src/Pages/Wishlist.js
@@ -8,12 +8,13 @@ import Row from "react-bootstrap/Row";
 import { Link } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import { AiOutlineHeart, AiOutlineDelete } from "react-icons/ai";
+import ReactStars from "react-stars";
 
 export default function Wishlist() {
   return (
     <>
       <LayOutNone>
-        <div className="container-fluid">
+        <div className="container">
           <Row>
             <Col>
               <div className="d-flex justify-content-start mt-2">
@@ -43,7 +44,7 @@ export default function Wishlist() {
                 <Card>
                   <Card.Header>Wishlist</Card.Header>
 
-                  <Card.Body>
+                  <Card.Body className="d-grid gap-3">
                     <Card className="w-100">
                       <Row>
                         <Col md={4} lg={3} className="d-inline">
@@ -68,14 +69,19 @@ export default function Wishlist() {
                                 }}
                                 className="row"
                               >
-                                <div className="col">
+                                <div className="col-6">
                                   <b className="fs-2">$100</b>
                                   <p className="fs-6 text-decoration-line-through">
                                     $300
                                   </p>
                                 </div>
+                                <div className="col-6">
+                                  <ReactStars value={4} />
+                                  <p>6000</p>
+                                </div>
+
                                 <div className="col">
-                                  <p className="mt-3 text-start">(65% OFF)</p>
+                                  <p className="text-start">(65% OFF)</p>
                                 </div>
                               </div>
                               <div
@@ -91,14 +97,224 @@ export default function Wishlist() {
                                     backgroundColor: "#7E90C8",
                                     color: "white",
                                   }}
-                                  className="col"
+                                  className="col-10"
                                 >
                                   Add to cart
                                 </Button>
                                 <Button
                                   variant="outline-light"
                                   size="lg"
-                                  className="col text-dark"
+                                  className="col-2 text-dark"
+                                  style={{ boxShadow: "none" }}
+                                >
+                                  <AiOutlineDelete />
+                                </Button>
+                              </div>
+                            </Card.Text>
+                          </Card.Body>
+                        </Col>
+                      </Row>
+                    </Card>
+                    <Card className="w-100">
+                      <Row>
+                        <Col md={4} lg={3} className="d-inline">
+                          <div style={{ width: "10rem" }}>
+                            <Card.Img
+                              variant="top"
+                              src="https://m.media-amazon.com/images/I/61ljxTBpTCL._SY450_.jpg"
+                            />
+                          </div>
+                        </Col>
+                        <Col md={8} lg={9}>
+                          <Card.Body>
+                            <Card.Title>
+                              boAt Rockerz 550 Bluetooth Wireless Over Ear
+                              Headphones
+                            </Card.Title>
+                            <Card.Text>
+                              <div
+                                style={{
+                                  display: "flex",
+                                  alignItems: "center",
+                                }}
+                                className="row"
+                              >
+                                <div className="col-6">
+                                  <b className="fs-2">$100</b>
+                                  <p className="fs-6 text-decoration-line-through">
+                                    $300
+                                  </p>
+                                </div>
+                                <div className="col-6">
+                                  <ReactStars value={4} />
+                                  <p>6000</p>
+                                </div>
+
+                                <div className="col">
+                                  <p className="text-start">(65% OFF)</p>
+                                </div>
+                              </div>
+                              <div
+                                style={{
+                                  alignItems: "center",
+                                }}
+                                className="row row-cols-auto d-flex justify-content-start"
+                              >
+                                <Button
+                                  variant="outline-light"
+                                  size="lg"
+                                  style={{
+                                    backgroundColor: "#7E90C8",
+                                    color: "white",
+                                  }}
+                                  className="col-10"
+                                >
+                                  Add to cart
+                                </Button>
+                                <Button
+                                  variant="outline-light"
+                                  size="lg"
+                                  className="col-2 text-dark"
+                                  style={{ boxShadow: "none" }}
+                                >
+                                  <AiOutlineDelete />
+                                </Button>
+                              </div>
+                            </Card.Text>
+                          </Card.Body>
+                        </Col>
+                      </Row>
+                    </Card>
+                    <Card className="w-100">
+                      <Row>
+                        <Col md={4} lg={3} className="d-inline">
+                          <div style={{ width: "10rem" }}>
+                            <Card.Img
+                              variant="top"
+                              src="https://m.media-amazon.com/images/I/61ljxTBpTCL._SY450_.jpg"
+                            />
+                          </div>
+                        </Col>
+                        <Col md={8} lg={9}>
+                          <Card.Body>
+                            <Card.Title>
+                              boAt Rockerz 550 Bluetooth Wireless Over Ear
+                              Headphones
+                            </Card.Title>
+                            <Card.Text>
+                              <div
+                                style={{
+                                  display: "flex",
+                                  alignItems: "center",
+                                }}
+                                className="row"
+                              >
+                                <div className="col-6">
+                                  <b className="fs-2">$100</b>
+                                  <p className="fs-6 text-decoration-line-through">
+                                    $300
+                                  </p>
+                                </div>
+                                <div className="col-6">
+                                  <ReactStars value={4} />
+                                  <p>6000</p>
+                                </div>
+
+                                <div className="col">
+                                  <p className="text-start">(65% OFF)</p>
+                                </div>
+                              </div>
+                              <div
+                                style={{
+                                  alignItems: "center",
+                                }}
+                                className="row row-cols-auto d-flex justify-content-start"
+                              >
+                                <Button
+                                  variant="outline-light"
+                                  size="lg"
+                                  style={{
+                                    backgroundColor: "#7E90C8",
+                                    color: "white",
+                                  }}
+                                  className="col-10"
+                                >
+                                  Add to cart
+                                </Button>
+                                <Button
+                                  variant="outline-light"
+                                  size="lg"
+                                  className="col-2 text-dark"
+                                  style={{ boxShadow: "none" }}
+                                >
+                                  <AiOutlineDelete />
+                                </Button>
+                              </div>
+                            </Card.Text>
+                          </Card.Body>
+                        </Col>
+                      </Row>
+                    </Card>
+                    <Card className="w-100">
+                      <Row>
+                        <Col md={4} lg={3} className="d-inline">
+                          <div style={{ width: "10rem" }}>
+                            <Card.Img
+                              variant="top"
+                              src="https://m.media-amazon.com/images/I/61ljxTBpTCL._SY450_.jpg"
+                            />
+                          </div>
+                        </Col>
+                        <Col md={8} lg={9}>
+                          <Card.Body>
+                            <Card.Title>
+                              boAt Rockerz 550 Bluetooth Wireless Over Ear
+                              Headphones
+                            </Card.Title>
+                            <Card.Text>
+                              <div
+                                style={{
+                                  display: "flex",
+                                  alignItems: "center",
+                                }}
+                                className="row"
+                              >
+                                <div className="col-6">
+                                  <b className="fs-2">$100</b>
+                                  <p className="fs-6 text-decoration-line-through">
+                                    $300
+                                  </p>
+                                </div>
+                                <div className="col-6">
+                                  <ReactStars value={4} />
+                                  <p>6000</p>
+                                </div>
+
+                                <div className="col">
+                                  <p className="text-start">(65% OFF)</p>
+                                </div>
+                              </div>
+                              <div
+                                style={{
+                                  alignItems: "center",
+                                }}
+                                className="row row-cols-auto d-flex justify-content-start"
+                              >
+                                <Button
+                                  variant="outline-light"
+                                  size="lg"
+                                  style={{
+                                    backgroundColor: "#7E90C8",
+                                    color: "white",
+                                  }}
+                                  className="col-10"
+                                >
+                                  Add to cart
+                                </Button>
+                                <Button
+                                  variant="outline-light"
+                                  size="lg"
+                                  className="col-2 text-dark"
                                   style={{ boxShadow: "none" }}
                                 >
                                   <AiOutlineDelete />
