85a7758c8aadffdad9cf1d8c85ba0f2bb2e37692
Daksh Mrigwani <dakshmrigwani@gmail.com>
2023-05-16 07:05:13 +0000

    Update data.js and ProductDetail.js

--- src/Pages/ProductDetail.js
+++ src/Pages/ProductDetail.js
@@ -197,7 +197,10 @@ function ProductDetail() {
                             <b>Price</b>
                           </Col>
                           <Col md={8} className="text-end">
-                            <b>$78.76</b>
+                            <b>
+                              <BsCurrencyRupee />
+                              {selectedProduct.DisPrice}
+                            </b>
                           </Col>
                         </Row>
                         <Row>
@@ -205,7 +208,10 @@ function ProductDetail() {
                             <b>Tax</b>
                           </Col>
                           <Col md={7} className="text-end">
-                            <b>$1.00</b>
+                            <b>
+                              <BsCurrencyRupee />
+                              50.00
+                            </b>
                           </Col>
                         </Row>
                         <Row>
@@ -213,7 +219,10 @@ function ProductDetail() {
                             <b>Delievery charge</b>
                           </Col>
                           <Col md={4} className="text-end">
-                            <b>$5.00</b>
+                            <b>
+                              <BsCurrencyRupee />
+                              50.00
+                            </b>
                           </Col>
                         </Row>
                         <hr />
@@ -222,7 +231,10 @@ function ProductDetail() {
                             <b>Total</b>
                           </Col>
                           <Col md={6} className="text-end">
-                            <b>$84.76</b>
+                            <b>
+                              <BsCurrencyRupee />
+                              84.76
+                            </b>
                           </Col>
                         </Row>
                         <div className="d-flex flex-column gap-2 mt-3">
