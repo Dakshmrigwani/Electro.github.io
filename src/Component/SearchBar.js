26f5333da062764db0ced098568ffe3fa5a9d14c
Daksh Mrigwani <dakshmrigwani@gmail.com>
2023-05-17 05:20:30 +0000

    Update 3 files

---
+++ src/Component/SearchBar.js
@@ -0,0 +1,29 @@
+import React,{useState} from "react"
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import { BsSearch } from "react-icons/bs";
+
+
+export default function SearchBar(){
+    return(
+        <>
+          <Form className="d-flex mr-auto">
+              <InputGroup>
+                <Form.Control
+                  placeholder="Search product name"
+                  aria-describedby="basic-addon1"
+                  className="increase_on_click"
+                />
+                <Button
+                  variant="outline-secondary"
+                  id="basic-addon1"
+                  style={{ boxShadow: "none" }}
+                >
+                  <BsSearch />
+                </Button>
+              </InputGroup>
+            </Form>
+        </>
+    )
+}
