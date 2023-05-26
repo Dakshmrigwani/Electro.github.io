1b9783eab8afad2929baafe39376084eb7641937
Daksh Mrigwani <dakshmrigwani@gmail.com>
2023-05-26 13:24:06 +0000

    basic is done

--- src/Component/SearchBar.js
+++ src/Component/SearchBar.js
@@ -3,12 +3,13 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
 import { BsSearch } from "react-icons/bs";
-
+import {ProductDetailData} from "./data"
 
 export default function SearchBar(){
+  const [query, setQuery] = useState("");
     return(
         <>
-          <Form className="d-flex mr-auto">
+          <Form className="d-flex mr-auto position-relative " onChange={(e)=> setQuery(e.target.value)}>
               <InputGroup>
                 <Form.Control
                   placeholder="Search product name"
@@ -24,6 +25,17 @@ export default function SearchBar(){
                 </Button>
               </InputGroup>
             </Form>
+            <div className="position-absolute end-0 top-100 ">
+              <div className="card">
+                <div className="card-body " style={{width:'500px',overflow:'auto'}}>
+                  {ProductDetailData.filter(product => product.LessTitle.toLowerCase().includes(query)).map((product)=>(
+                    <div key={product.id}>
+                      <div className="">{product.LessTitle}</div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              </div>
         </>
     )
 }
