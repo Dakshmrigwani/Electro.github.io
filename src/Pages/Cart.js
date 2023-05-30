03aab3000f3dacde0a4796a5e639e74c83d2685a
Daksh Mrigwani <dakshmrigwani@gmail.com>
2023-05-30 08:08:46 +0000

    Update Cart.js

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -2,8 +2,9 @@ import React, { createContext } from "react";
 import { CartList } from "../Component/data";
 import CartContext from "./CartContext"
 
-
 export const cartContext = createContext()
+
+
 export default function Cart() {
 
   return (
