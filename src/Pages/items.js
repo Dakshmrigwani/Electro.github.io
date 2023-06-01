b54676af1ca278551e7ac49dbf1ca251e277399b
Daksh Mrigwani <dakshmrigwani@gmail.com>
2023-06-01 10:58:35 +0000

    now working just have a problem in quantity

--- src/Pages/items.js
+++ src/Pages/items.js
@@ -1,13 +1,5 @@
-const cartItems = [];
-
-const addToCart = (value) => {
-  const itemExists = cartItems.some((item) => item.id === value.id);
-
-  if (!itemExists) {
-    cartItems.push(value);
-    console.log(cartItems, "Added to cart");
-  }
-};
+// Load cart items from localStorage
+const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
 
 const removeFromCart = (value) => {
   const itemIndex = cartItems.findIndex((item) => item.id === value.id);
@@ -15,11 +7,27 @@ const removeFromCart = (value) => {
   if (itemIndex !== -1) {
     cartItems.splice(itemIndex, 1);
     console.log(cartItems, "Removed from cart");
+    saveCartItemsToLocalStorage(); // Save updated cart items to localStorage
+  }
+};
+
+const addToCart = (value) => {
+  const itemExists = cartItems.some((item) => item.id === value.id);
+
+  if (!itemExists) {
+    cartItems.push(value);
+    console.log(cartItems, "Added to cart");
+    saveCartItemsToLocalStorage(); // Save updated cart items to localStorage
   }
 };
 
 const items = () => {
-  // Your code for the 'items' function
+  return cartItems;
+};
+
+// Save cart items to localStorage
+const saveCartItemsToLocalStorage = () => {
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
 };
 
 export { cartItems, addToCart, removeFromCart };
