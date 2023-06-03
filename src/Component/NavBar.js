f7b90220020b562bcda8e4a316f22c3fac1d5f7d
Daksh Mrigwani <dakshmrigwani@gmail.com>
2023-06-03 16:15:54 +0000

    navbar fixed

--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -5,7 +5,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import InputGroup from "react-bootstrap/InputGroup";
 import { BsCart3, BsMoonStarsFill, BsSunFill, BsSearch } from "react-icons/bs";
-import SearchBar from "./SearchBar"
+import SearchBar from "./SearchBar";
 import {
   AiOutlineHeart,
   AiOutlineUserAdd,
@@ -15,61 +15,63 @@ import { Link } from "react-router-dom";
 import React from "react";
 
 function NavBar() {
-  
   return (
     <>
       <Navbar bg="light" expand="lg" className="sticky-top">
         <Container fluid>
-          <Navbar.Brand>
-            <Link to="/Pages/Body" className="text-decoration-none text-dark">
-              Electro.
-            </Link>
-            
-          </Navbar.Brand>
-          <div className="d-flex d-lg-none justify-content-center ">
-              <SearchBar className="increase_on_click"/>
-            </div>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0 "
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Link
-                to="/Pages/Headphone"
-                className="text-decoration-none text-dark me-2"
-              >
-                Headphone
-              </Link>
-              <Link
-                to="/Pages/Laptops"
-                className="text-decoration-none text-dark me-2"
-              >
-                Laptops
-              </Link>
-              <Link
-                to="/Pages/Speaker"
-                className="text-decoration-none text-dark me-2"
-              >
-                Speakers
-              </Link>
-              <Link
-                to="/Pages/Watch"
-                className="text-decoration-none text-dark me-2"
-              >
-                Watches
-              </Link>
-              <Link
-                to="/Pages/Smartphones"
-                className="text-decoration-none text-dark me-2"
-              >
-                Smartphones
-              </Link>
-            </Nav>
-            <span className="d-none d-lg-flex">
-            <SearchBar className="increase_on_click"/>
+          <div className="d-flex">
+            <span className="order-2 order-lg-1">
+              <Navbar.Brand>
+                <Link
+                  to="/Pages/Body"
+                  className="text-decoration-none text-dark"
+                >
+                  Electro.
+                </Link>
+              </Navbar.Brand>
             </span>
+            <span className="order-1 order-lg-2">
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                <Nav
+                  className="me-auto my-2 my-lg-0"
+                  style={{ maxHeight: "120px" }}
+                >
+                  <Link
+                    to="/Pages/Headphone"
+                    className="text-decoration-none text-dark me-2"
+                  >
+                    Headphone
+                  </Link>
+                  <Link
+                    to="/Pages/Laptops"
+                    className="text-decoration-none text-dark me-2"
+                  >
+                    Laptops
+                  </Link>
+                  <Link
+                    to="/Pages/Speaker"
+                    className="text-decoration-none text-dark me-2"
+                  >
+                    Speakers
+                  </Link>
+                  <Link
+                    to="/Pages/Watch"
+                    className="text-decoration-none text-dark me-2"
+                  >
+                    Watches
+                  </Link>
+                  <Link
+                    to="/Pages/Smartphones"
+                    className="text-decoration-none text-dark me-2"
+                  >
+                    Smartphones
+                  </Link>
+                </Nav>
+              </Navbar.Collapse>
+            </span>
+          </div>
+          <span className="d-flex d-sm-none">
             <Button className="btn btn-light" style={{ boxShadow: "none" }}>
               <Link to="/Pages/Wishlist">
                 <AiOutlineHeart className="text-dark fs-4" />
@@ -88,7 +90,32 @@ function NavBar() {
                 <AiOutlineUser />
               </Link>
             </Button>
-          </Navbar.Collapse>
+          </span>
+          <div className="d-flex gap-3">
+            <span className="d-flex justify-content-center order-2 order-sm-1">
+              <SearchBar className="increase_on_click" />
+            </span>
+            <div className="d-none d-sm-flex justify-content-end order-1 order-sm-2">
+              <Button className="btn btn-light" style={{ boxShadow: "none" }}>
+                <Link to="/Pages/Wishlist">
+                  <AiOutlineHeart className="text-dark fs-4" />
+                </Link>
+              </Button>
+              <Button className="btn btn-light" style={{ boxShadow: "none" }}>
+                <Link to="/pages/Cart">
+                  <BsCart3 className="text-dark fs-4" />
+                </Link>
+              </Button>
+              <Button
+                className="btn btn-light fs-5"
+                style={{ boxShadow: "none" }}
+              >
+                <Link to="/pages/SignIn">
+                  <AiOutlineUser />
+                </Link>
+              </Button>
+            </div>
+          </div>
         </Container>
       </Navbar>
     </>
