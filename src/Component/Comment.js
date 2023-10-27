c99d52396cf1e2ce7ddfa136138734fd5a3fcbdf
Daksh Mrigwani <86098238+Dakshmrigwani@users.noreply.github.com>
2023-10-27 09:55:55 +0000

    working on comments later

--- src/Component/Comment.js
+++ src/Component/Comment.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import image1 from "../Images/image1.avif";
 import Button from "react-bootstrap/Button";
+import { BsFillPencilFill } from "react-icons/bs";
+import { AiFillDelete } from "react-icons/ai";
 import { useSelector, useDispatch } from "react-redux";
 import { AddComment, DeleteComment, editComment } from "../Slices/CommentSlice";
 
@@ -18,7 +20,9 @@ const Comments = () => {
         const parsedComments = JSON.parse(storedComments);
         if (Array.isArray(parsedComments)) {
           // Filter out invalid comments
-          const validComments = parsedComments.filter(comment => comment.id && comment.content);
+          const validComments = parsedComments.filter(
+            (comment) => comment.id && comment.content,
+          );
           dispatch(AddComment(validComments));
         } else {
           console.error("Stored comments are not in the expected format.");
@@ -48,90 +52,90 @@ const Comments = () => {
     dispatch(DeleteComment(id));
   };
   console.log(comments);
-  return (
-    <div className="">
-      <h3>Comments</h3>
-      <div className="">
-        <div className="card mb-3 border-0">
-          <div className="row g-0">
-            <div className="col-4">
-              <img
-                src={image1}
-                className="img-fluid rounded-start h-100 object-fit-cover"
-                alt="..."
-              />
-            </div>
-            <div className="col-8">
-              <div class="statictext">
-                <div className="card-body">
-                  <h5 className="card-title">Sherlyn</h5>
-                  <p className="card-text">
-                    I am using this product around 5 years and its fine and
-                    value of money
-                  </p>
-                  {/* <div className="d-flex justify-content-end align-items-center gap-3">
-                              <button className="btn">
-                                <BsFillPencilFill/>
-                              </button>
-                              <button className="btn">
-                                <AiFillDelete/>
-                              </button>
-                            </div> */}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="addnew">
-          <div className="form-floating">
-            {/* {comments.map((body) => (
-              <div key={body.id} className="card mb-3 border-0">
-                <div className="card-body">
-                  {editCommentId === body.id ? (
-                    <>
-                      <textarea
-                        value={editedComment}
-                        onChange={(e) => setEditedComment(e.target.value)}
-                      ></textarea>
-                      <button onClick={handleEditComment}>Save</button>
-                    </>
-                  ) : (
-                    <>
-                      <h5 className="card-title">Sherlyn</h5>
-                      <p className="card-text">{body.content}</p>
-                      <button onClick={() => setEditCommentId(body.id)}>
-                        Edit
-                      </button>
-                      <button onClick={() => handleDeleteComment(body.id)}>
-                        Delete
-                      </button>
-                    </>
-                  )}
-                </div>
-              </div>
-            ))} */}
-            <div className="addnew">
-              <div className="form-floating">
-                <textarea
-                  style={{ height: "100px" }}
-                  className="form-control"
-                  placeholder="Leave a comment here"
-                  id="floatingTextarea"
-                  value={newComment}
-                  onChange={(e) => setNewComment(e.target.value)}
-                ></textarea>
-                <label htmlFor="floatingTextarea">Add new Comment</label>
-              </div>
-              <div className="mt-2">
-                <button className="btn btn-primary" onClick={handlePostComment}>
-                  Post Your View
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  // return (
+  //     <div className="">
+  //       <h3>Comments</h3>
+  //       <div className="">
+  //         <div className="card mb-3 border-0">
+  //           <div className="row g-0">
+  //             <div className="col-4">
+  //               <img
+  //                 src={image1}
+  //                 className="img-fluid rounded-start h-100 object-fit-cover"
+  //                 alt="..."
+  //               />
+  //             </div>
+  //             <div className="col-8">
+  //               <div class="statictext">
+  //                 <div className="card-body">
+  //                   <h5 className="card-title">Sherlyn</h5>
+  //                   <p className="card-text">
+  //                     I am using this product around 5 years and its fine and
+  //                     value of money
+  //                   </p>
+  //                   <div className="d-flex justify-content-end align-items-center gap-3">
+  //                     <button className="btn">
+  //                       <BsFillPencilFill />
+  //                     </button>
+  //                     <button className="btn">
+  //                       <AiFillDelete />
+  //                     </button>
+  //                   </div>
+  //                 </div>
+  //               </div>
+  //             </div>
+  //           </div>
+  //         </div>
+  //         <div className="addnew">
+  //           <div className="form-floating">
+  //             {/* {comments.map((body) => (
+  //               <div key={body.id} className="card mb-3 border-0">
+  //                 <div className="card-body">
+  //                   {editCommentId === body.id ? (
+  //                     <>
+  //                       <textarea
+  //                         value={editedComment}
+  //                         onChange={(e) => setEditedComment(e.target.value)}
+  //                       ></textarea>
+  //                       <button onClick={handleEditComment}>Save</button>
+  //                     </>
+  //                   ) : (
+  //                     <>
+  //                       <h5 className="card-title">Sherlyn</h5>
+  //                       <p className="card-text">{body.content}</p>
+  //                       <button onClick={() => setEditCommentId(body.id)}>
+  //                         Edit
+  //                       </button>
+  //                       <button onClick={() => handleDeleteComment(body.id)}>
+  //                         Delete
+  //                       </button>
+  //                     </>
+  //                   )}
+  //                 </div>
+  //               </div>
+  //             ))} */}
+  //             <div className="addnew">
+  //               <div className="form-floating">
+  //                 <textarea
+  //                   style={{ height: "100px" }}
+  //                   className="form-control"
+  //                   placeholder="Leave a comment here"
+  //                   id="floatingTextarea"
+  //                   value={newComment}
+  //                   onChange={(e) => setNewComment(e.target.value)}
+  //                 ></textarea>
+  //                 <label htmlFor="floatingTextarea">Add new Comment</label>
+  //               </div>
+  //               <div className="mt-2">
+  //                 <button className="btn btn-primary" onClick={handlePostComment}>
+  //                   Post Your View
+  //                 </button>
+  //               </div>
+  //             </div>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  // );
 };
 export default Comments;
